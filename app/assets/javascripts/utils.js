90f8d301d0639f39df18d6d2e0d879d3bad4070c
Carlos Ramos <carakan@gmail.com>
2016-08-19 18:17:25 -0400

    added timeout for enable reloading without websockets

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -3,5 +3,7 @@ $(document).ready(function(){
   $(".table.disabled_false .elem").click(function(){
     $.get("/games/" + $(this).data("cords"))
   })
+
+  setTimeout(function(){window.location.reload(false);}, 5000)
 })
 
